004475df1c13cb19b40b64a0df64d0a72271d78f
Dmitry_71 <dgorodilov@mail.ru>
2024-04-25 16:59:11 +0300

    Stub and tests for add

--- src/main/java/org/example/PhoneBook.java
+++ src/main/java/org/example/PhoneBook.java
@@ -1,5 +1,7 @@
 package org.example;
 
 public class PhoneBook {
-
+    public static int add(String name, long number) {
+        return 0;
+    }
 }
--- src/test/java/PhoneBookTest.java
+++ src/test/java/PhoneBookTest.java
@@ -1,3 +1,20 @@
+import org.example.PhoneBook;
+import org.junit.jupiter.api.RepeatedTest;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class PhoneBookTest {
-    
+
+    @RepeatedTest(2) 
+    public void testAdd_result() {
+        // given:
+        final String name = "TestName";
+        final long number = 9999999999L;
+        final int original = 1;
+        // when:
+        final int result = PhoneBook.add(name, number);
+        // then:
+        assertEquals(original, result);
+    }
+
 }
